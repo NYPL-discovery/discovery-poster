fc05e09612362a7570c5ca92a316dea3b05fbb50
danamansana <danielappel@nypl.org>
2026-06-16 11:48:45 -0400

    Update to node 24:

--- provisioning/base/resources.tf
+++ provisioning/base/resources.tf
@@ -55,7 +55,7 @@ resource "aws_lambda_function" "lambda_instance" {
   handler       = "index.handler"
   memory_size   = 512
   role          = "arn:aws:iam::946183545209:role/lambda-full-access"
-  runtime       = "nodejs20.x"
+  runtime       = "nodejs24.x"
   timeout       = 300
 
   # Location of the zipped code in S3:
